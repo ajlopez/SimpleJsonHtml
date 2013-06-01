bcedf75dac279077427dbfc747eaa45465bf5416
ajlopez <webmaster@ajlopez.com>
2013-06-01 11:04:21 -0300

    Transform h1 with model

--- test/transform.js
+++ test/transform.js
@@ -17,3 +17,8 @@ var result = sjh.transform("Hello ${name}", { name: 'Adam' });
 assert.ok(result);
 assert.equal(result, "Hello Adam");
 
+var result = sjh.transform({ h1: "Hello ${name}" }, { name: 'Adam' });
+
+assert.ok(result);
+assert.equal(result, "<h1>Hello Adam</h1>");
+
