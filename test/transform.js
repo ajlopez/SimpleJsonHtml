5570b64c4ba58db064ccf3ab3d2e9188320d4d44
ajlopez <webmaster@ajlopez.com>
2013-06-01 10:53:07 -0300

    Second transform test, using string as template

--- test/transform.js
+++ test/transform.js
@@ -1,6 +1,11 @@
 
 var sjh = require('..'),
     assert = require('assert');
+    
+var result = sjh.transform("Hello", null);
+
+assert.ok(result);
+assert.equal(result, "Hello");
 
 var result = sjh.transform({ h1: "Hello" }, null);
 
